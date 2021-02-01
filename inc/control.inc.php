5eed128aab9394a802ec915a1f318108b58ab530
Malonel <95588318+Malonel@users.noreply.github.com>
2021-02-01 13:10:20 +0000

    travis.yml updated

--- inc/control.inc.php
+++ inc/control.inc.php
@@ -1,4 +1,9 @@
 <?php
 if (isset($index_call)) {
   if (!empty($page)) {
-    if (file_exists("./inc/pa
+    if (file_exists("./inc/pages/$page.inc.php")) {
+      require_once("./inc/pages/$page.inc.php");
+    } else {
+	  echo "<p>The requested page was not found, sorry! :(</p>";  
+    }
+  } el
