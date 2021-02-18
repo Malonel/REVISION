ab7c71c538f4bb4183539452c6425444fc3680fc
Malonel <95588318+Malonel@users.noreply.github.com>
2021-02-18 16:51:52 +0000

    Update schulung.md

--- inc/pages/mempool.inc.php
+++ inc/pages/mempool.inc.php
@@ -4,4 +4,14 @@
 <table class="table table-striped table-condensed">
 <tr>
   <th>交易编号</th>
-  <th>发送数量<
+  <th>发送数量</th>
+</tr>
+
+<?php
+$tx_ids = $_SESSION[$rpc_client]->getrawmempool();
+
+if (empty($tx_ids)) {
+  echo "<tr><td colspan='2'>当前内存池为空.</td></tr>";
+} else {
+
+	$tx = array(
