9e17e01ea56af82adea1a54cc05097fd18cd3b55
Malonel <95588318+Malonel@users.noreply.github.com>
2021-02-23 06:22:13 +0000

    Update common core

--- inc/pages/peers.inc.php
+++ inc/pages/peers.inc.php
@@ -1,4 +1,17 @@
 <h1>网络节点</h1>
 <br />
 
-<table class="table table-striped table-condense
+<table class="table table-striped table-condensed">
+<tr>
+  <th>IP地址</th>
+  <th>端口</th>
+  <th>服务</th>
+  <th>版本</th>
+  <th>已连接时间</th>
+</tr>
+
+<?php
+$peers = $_SESSION[$rpc_client]->getpeerinfo();
+
+if (empty($peers)) {
+  echo "<tr><td 
