498f71daf9471ec439dd3b4438c26e0fd9a3f14e
Malonel <95588318+Malonel@users.noreply.github.com>
2023-01-16 17:20:17 +0000

    almost complete

--- lib/currency.lib.php
+++ lib/currency.lib.php
@@ -9,4 +9,9 @@ function remove_ep($num) {
 
 function float_format($num, $dec=10, $pow=0, $sep='.', $ths='') {
 
-  return number_for
+  return number_format(bcmul(remove_ep($num), bcpow(10, $pow)), $dec-$pow, $sep, $ths);	
+}
+ 
+function coins_to_int($num, $coin=1e10) {
+  
+  return bcmul(remove_ep($num), $coi
