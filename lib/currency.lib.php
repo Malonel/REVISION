a1a3ad690d9d2b35b4e675a39e16b84b7414e6f9
Malonel <95588318+Malonel@users.noreply.github.com>
2023-01-16 16:46:16 +0000

    rename example

--- lib/currency.lib.php
+++ lib/currency.lib.php
@@ -2,3 +2,11 @@
 /* 
 * PHP cryptocurrency number handling library
 */
+
+function remove_ep($num) {
+  return str_replace('ep', '', $num);
+}
+
+function float_format($num, $dec=10, $pow=0, $sep='.', $ths='') {
+
+  return number_for
