d5a627c451f6bf9866fca857fd053b34ba630241
Malonel <95588318+Malonel@users.noreply.github.com>
2023-01-18 06:46:39 +0000

    added message

--- lib/rpcclient.lib.php
+++ lib/rpcclient.lib.php
@@ -1,2 +1,12 @@
 <?php
 /*
+A simple class for making calls to Bitcoin-based API's using PHP.
+https://github.com/aceat64/EasyBitcoin-PHP
+
+====================
+
+The MIT License (MIT)
+
+Copyright (c) 2013 Andrew LeCody
+
+Pe
